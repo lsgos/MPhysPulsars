95a75ba06b93a1e1b725ec403867a4c3f1027a48
lewis smith <lgsmith271@gmail.com>
2016-11-01 15:57:18 +0000

    parallelised the classifier evaluation to speed it up

--- src/evaluate_classifier.py
+++ src/evaluate_classifier.py
@@ -5,12 +5,22 @@ and saving fully trained classifiers to disk using joblib.
 """
 from __future__ import division
 from __future__ import print_function
+
 import argparse
+import os
+
 from JACS_utils.ARFF import ARFF
 from JACS_utils.ClassifierStats import ClassifierStats
 
+from joblib import Parallel, delayed
+
+from sklearn.base import clone
+from sklearn.externals import joblib
+
+
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import confusion_matrix
+
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.naive_bayes import GaussianNB
@@ -20,7 +30,8 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import AdaBoostClassifier
 
-from sklearn.externals import joblib
+
+
 
 def print_metrics(metrics):
     metric_labels = ["G-Mean","F-Score","Recall","Precision","Specificity","FPR","Accuracy"]
@@ -43,28 +54,33 @@ def get_metrics(C, datalen):
     metrics.append(C.getAccuracy())
     return metrics
 
-def calculate_metrics(clf, k_folds, x, y, shuffle = True):
-    skf = StratifiedKFold(n_splits = k_folds, shuffle = shuffle)
-    metrics = None
-    for train_index, test_index in skf.split(x,y):
-        x_train, x_test = x[train_index], x[test_index]
-        y_train, y_test = y[train_index], y[test_index]
+def calculate_metrics(classifier, k_folds, x, y, n_jobs,shuffle = True):
 
-        clf.fit(x_train, y_train)
-        #calculate cross validation metrics
-        y_pred = clf.predict(x_test)
-        cm = confusion_matrix(y_test, y_pred)
-        stats = ClassifierStats(cm)
-        stats.calculate()
-        kmetrics = get_metrics(stats, len(y_test))
-        if metrics is None:
-            metrics = kmetrics
-        else:
-            metrics = [m + k for (m,k) in zip(metrics, kmetrics)]
-    metrics = [metric / k_folds for metric in metrics]
-    #average metrics over cross validation
+    skf = StratifiedKFold(n_splits = k_folds, shuffle = shuffle)
+    metrics = Parallel(n_jobs = n_jobs)(delayed(_calculate_metrics)(classifier,x,y,split) for split in skf.split(x,y))
+    return map(lambda x: sum(x) / len(x), zip(*metrics))
+
+def _calculate_metrics(classifier,x,y, split_tup):
+    train_index, test_index = split_tup
+
+    clf = clone(classifier)
+
+    clf = clone(classifier) #make sure they are not modified outside the loop
+
+    x_train, x_test = x[train_index], x[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+
+    clf.fit(x_train, y_train)
+    #calculate cross validation metrics
+    y_pred = clf.predict(x_test)
+    cm = confusion_matrix(y_test, y_pred)
+    stats = ClassifierStats(cm)
+    stats.calculate()
+    metrics = get_metrics(stats, len(y_test))
     return metrics
 
+def _paralleliser(clf_tup):
+    return (name,calculate_metrics(clf, args.k_folds, train_x, train_y))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = "Evaluates the perfomance of several classifiers on a pulsar dataset in .arff format\n. \
@@ -79,7 +95,7 @@ if __name__ == "__main__":
     parser.add_argument("--ignore_period", type = bool, default = True, help = "By default, assume the period is the first field of the arff file, and ignore it (default True)")
     parser.add_argument("--save_classifiers", help = "Save trained classifiers to disk using joblib in this path location if provided")
     parser.add_argument("--k_folds","-k", default = 5, help = "Number of folds to use for cross validation (default 5)")
-
+    parser.add_argument("--n_jobs","-j", default = 4, help = "Number of cores to parallelise over")
     args = parser.parse_args()
 
     arff_reader = ARFF()
@@ -91,16 +107,23 @@ if __name__ == "__main__":
 
     #fit classifiers
     classifiers = []
-    classifiers.append(("CART tree",DecisionTreeClassifier() ) )
+    classifiers.append(("CART_tree",DecisionTreeClassifier() ) )
     classifiers.append(("MLP",Pipeline([('scaler', StandardScaler()),('mlp',MLPClassifier())]) ) )
-    classifiers.append(("Naive Bayes",GaussianNB() ) )
+    classifiers.append(("Naive_Bayes",GaussianNB() ) )
     classifiers.append(("SVM",Pipeline([('scaler', StandardScaler()), ('svc',SVC(class_weight = 'balanced') )])))
-    classifiers.append(("Random Forest",RandomForestClassifier() ))
+    classifiers.append(("Random_Forest",RandomForestClassifier() ))
     classifiers.append(("AdaBoost", AdaBoostClassifier() ))
-    metrics = [(name,calculate_metrics(clf, args.k_folds, train_x, train_y) ) for name, clf in classifiers]
+    metrics = [(name,calculate_metrics(clf, args.k_folds, train_x, train_y, args.n_jobs) ) for name, clf in classifiers]
     #print metrics
     for name, metric in metrics:
         print("{}:".format(name))
         print_metrics(metrics)
 
+
     if args.save_classifiers is not None:
+        if not os.path.exists(args.save_classifiers):
+            raise IOError("Cannot find path specified to save classifiers to")
+        for name,clf in classifiers:
+            #fit on the whole training set and save the classifier
+            clf.fit(train_x,train_y)
+            joblib.dump(clf, os.path.join(args.save_classifiers,(name+".pkl")))
