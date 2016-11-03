7f18d92af24bb8f9475dcea0acf0a9b7005868c8
lewis smith <lgsmith271@gmail.com>
2016-11-03 10:56:18 +0000

    created some new split datasets, added help to arff_cat.py, removed an obselete file and renamed arff_splitter.py

--- src/arff_cat.py
+++ src/arff_cat.py
@@ -8,6 +8,7 @@ Created on Thu Oct 27 09:24:33 2016
 # Code that concatenates a .arff file
 from __future__ import print_function
 import argparse
+import random
 
 def parse_arff_header(f):
     header = []
@@ -27,8 +28,11 @@ def parse_arff_header(f):
     return header
 
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description = "Concatenates two arff files into \
+        a single file. Assumes that they share a header: if this is not the case\
+        the output will not be a valid arff file. Prints to stdout")
 parser.add_argument('files', nargs='+', help = 'list of files to concatenate')
+parser.add_argument('--shuffle','-s', type = bool, default = False, help = "If set to true, shuffle the order of the data points")
 
 args = parser.parse_args()
 header = None
@@ -46,6 +50,8 @@ for filename in args.files:
             fields = line.split(',')
             lines.append(line)
 
+if args.shuffle:
+    random.shuffle(lines)
 
 for head in header:
     print (head, end = "")
