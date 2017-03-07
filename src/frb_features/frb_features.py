7689fa3610a266c97d535e524f533e7126834757
lewis smith <lgsmith271@gmail.com>
2017-03-07 15:29:43 +0000

    added more features, fixed a serious bug related to snr filtering causing the labels to be misaligned

--- src/frb_features/frb_features.py
+++ src/frb_features/frb_features.py
@@ -8,6 +8,7 @@ import argparse
 from scipy.stats import skew, kurtosis
 import frb_cand
 import numpy as np
+import functools
 
 CAND_FILE_RE = re.compile(r".*labelled_dataset.dat$")
 
@@ -15,7 +16,7 @@ class FRB_file(object):
     """
     read and represent a frb candidate file
     """
-    def __init__(self, filename, n_bins=10):
+    def __init__(self, filename, n_bins=10, filter_zero_dm = False):
         data, labels = frb_cand.read(filename)
         self.n_bins = n_bins
         self.fields = ["snr",
@@ -32,43 +33,63 @@ class FRB_file(object):
               "primary_beam",
               "max_snr",
               "beam_num"]
-        self.n_n_window = 5
-        data = data[data[:,0] != np.inf, :] #remove any datapoints with infinite snr
+        self.n_n_windows = [10 ** x for x in np.linspace(0,2,10)]
+        
+        
+        non_inf_inds = data[:,0] != np.inf
+        data = data[non_inf_inds, :] #remove any datapoints with infinite snr
+        labels = labels[non_inf_inds]
+        
+        if filter_zero_dm:
+        	dm_greater_zero_inds = data[:,self.fields.index("dm")] > 0.
+         	data = data[dm_greater_zero_inds]
+         	labels = labels[dm_greater_zero_inds]
+        
+      
+        
         self.dat = data
         self.labels = labels
     def _calculate_dm(self):
         #returns an array of just the dm values
         return self.dat[:,self.fields.index("dm")]
+        
     def _calculate_log_dm(self):
     	return np.log(self._calculate_dm())
+    	
     def _calculate_log_width(self):
-    	return np.log(self._calculate_width())		
+    	return np.log(self._calculate_width())	
+    		
     def _calculate_width(self):
         #box width = 2 ^ (filter_width), so the filter width should contain the same information, if not slightly more 
         return self.dat[:,self.fields.index("filter_width")]
         
-    def _calculate_num_neighbours(self):
+    def _calculate_num_neighbours(self, box_size):
         """
         This is a heuristic me, mike, mat and alex have come up with: rfi often occurs in 'towers', so 
         for each file, record the number of points that occured within +/- a few seconds as a score
         """
-        return np.apply_along_axis(self._get_neighbour, 1, self.dat)
+        return np.apply_along_axis(functools.partial(self._get_neighbour, box_size) , 1, self.dat)
 
-    def _get_neighbour(self, datapoint):
+    def _get_neighbour(self, box_size, datapoint):
         time_pos = datapoint[self.fields.index("time_of_peak")]
-        time_low = time_pos - self.n_n_window
-        time_hi = time_pos + self.n_n_window
+        time_low = time_pos - box_size
+        time_hi = time_pos + box_size
         time_ind = self.fields.index("time_of_peak")
         return np.logical_and( time_low < self.dat[:,time_ind], self.dat[:,time_ind] < time_hi).astype(np.int).sum()
+        
     def get_features(self):
         """
         returns the features as an iterator in string format, for easy writing to a file
         """
         feature_calcs = [self._calculate_dm, 
         self._calculate_width, 
-        self._calculate_num_neighbours, 
-        self._calculate_log_dm,
-        self._calculate_log_width] #this defines the order these will be called in 
+        #self._calculate_log_dm,
+        #self._calculate_log_width,
+        ] 
+        
+        #add nearest neightbour scores at various widths to the calculation
+        feature_calcs += [ functools.partial(self._calculate_num_neighbours, window) for window in self.n_n_windows]   
+        #this defines the order these will be called in 
         features = [feature() for feature in feature_calcs]
         features.append(list(self.labels)) #add the label
         for cand_feats in zip(*features):
